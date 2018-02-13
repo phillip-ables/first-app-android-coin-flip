9ae5d52a4448d6bb447d6924ec0192f970618cd7
Phillip <phillipables1@gmail.com>
2018-02-13 09:18:14 -0500

    implement btn onclick method to toast a text

--- Decider/app/src/main/java/com/example/techtron/decider/MainActivity.java
+++ Decider/app/src/main/java/com/example/techtron/decider/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.techtron.decider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnFate = (Button)findViewById(R.id.btn_flip_activity);
+        btnFate.setOnClickListener(this);
+    }
+
+    //implement the OnClickListener callback
+    public void onClick(View view) {
+        //do something
+        Toast.makeText(getApplicationContext(), "text", Toast.LENGTH_SHORT).show();
+
     }
 }
