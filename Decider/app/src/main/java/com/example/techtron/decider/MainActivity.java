efe850da25723e1394ff3f928e67c8977ecf5b23
Phillip <phillipables1@gmail.com>
2018-02-13 12:22:48 -0500

    textview undecided changes to head or tail on click result

--- Decider/app/src/main/java/com/example/techtron/decider/MainActivity.java
+++ Decider/app/src/main/java/com/example/techtron/decider/MainActivity.java
@@ -5,15 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //i need all my variables
+    TextView textView;
+    Button btnFate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnFate = (Button)findViewById(R.id.btn_flip_activity);
+        btnFate = new Button(this);
+        textView = new TextView(this);
+
+        textView = (TextView)findViewById(R.id.textView);
+        btnFate = (Button)findViewById(R.id.btn_flip_activity);
+
+        textView.setText(R.string.undecided);
         btnFate.setOnClickListener(this);
     }
 
@@ -23,9 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Random fate = new Random();
         //returns one or zero
         int fatesNumber = fate.nextInt(2);
-        if(fatesNumber < 0.5)
-            Toast.makeText(getApplicationContext(), "Tails: "+fatesNumber, Toast.LENGTH_SHORT).show();
-        else
+        if (fatesNumber < 0.5) {
+            textView.setText(R.string.head);
+            Toast.makeText(getApplicationContext(), "Tails: " + fatesNumber, Toast.LENGTH_SHORT).show();
+        } else
+        {
+            textView.setText(R.string.tails);
             Toast.makeText(getApplicationContext(), "Heads: " + fatesNumber, Toast.LENGTH_SHORT).show();
+        }
     }
 }
