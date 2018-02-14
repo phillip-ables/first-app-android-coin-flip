7d96d533e9c5d9698d8e1500c395ff449eaaf528
Phillip <phillipables1@gmail.com>
2018-02-13 23:28:45 -0500

    set tails background and changes text color to something more visable

--- Decider/app/src/main/java/com/example/techtron/decider/MainActivity.java
+++ Decider/app/src/main/java/com/example/techtron/decider/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.techtron.decider;
 import java.util.Random;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,7 +37,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Random fate = new Random();
         //returns one or zero
         int fatesNumber = fate.nextInt(2);
-        btnFate.setAlpha(0.5f)
+        btnFate.setAlpha(0.5f);
+        textView.setTextColor(Color.parseColor("#8A0707"));
         if (fatesNumber < 0.5) {
             textView.setText(R.string.head);
             mLayout.setBackgroundDrawable(getResources().getDrawable(R.drawable.doit));
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else
         {
             textView.setText(R.string.tails);
+            mLayout.setBackgroundDrawable(getResources().getDrawable(R.drawable.dont));
             Toast.makeText(getApplicationContext(), "Heads: " + fatesNumber, Toast.LENGTH_SHORT).show();
         }
     }
